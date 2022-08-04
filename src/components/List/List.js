33908799815a17e45b23298f24e82b27b45a7187
Adam Kwidzyński <adamkwidzynski@gmail.com>
2022-08-05 00:37:29 +0200

    task 15.5 v1

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,23 +1,20 @@
-const List = () => {
-  return (
-    <div>
-        <header>
-            <h2>Things to do<span>soon!</span></h2>
-        </header>
-        <p>Interesting things I want to check out</p>
-        <section>
-            <article>
-                <h2>Books</h2>
-            </article>
-            <article>
-                <h2>Movies</h2>
-            </article>
-            <article>
-                <h2>Games</h2>
-            </article>
-        </section>
-    </div>
-  );
-};
+import styles from './List.module.scss';
+import Column from '../Column/Column';
 
-export default List;
+const List = () => {
+    return (
+        <div className= {styles.list}>
+        <header className = {styles.header}>
+            <h2 className = {styles.title}>Things to do<span>soon</span></h2>
+        </header>
+        <p className = {styles.description}>Interesting things I want to check out</p>
+        <section className = {styles.columns}>
+            <Column icon= "book" title= "Books" />
+            <Column icon= "film" title="Movies" />
+            <Column icon= "gamepad" title= "Games" />
+        </section>
+      </div>
+    );
+  };
+
+  export default List; 
